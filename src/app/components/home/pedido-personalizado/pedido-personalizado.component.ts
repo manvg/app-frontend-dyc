825433e08da833c6fb073829f0133ffe5c835766
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-07-06 11:46:28 -0400

    Nueva redirección desde home hacia solicitud personalizada

--- src/app/components/home/pedido-personalizado/pedido-personalizado.component.ts
+++ src/app/components/home/pedido-personalizado/pedido-personalizado.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-pedido-personalizado',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './pedido-personalizado.component.html',
   styleUrls: ['./pedido-personalizado.component.scss']
 })
